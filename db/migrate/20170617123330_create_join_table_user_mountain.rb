b2de5607c2ad3cde0747a0fd5b87ef042b365019
Quinn Stobbs <q.stobbs@gmail.com>
2017-06-19 15:20:33 +1000

    changed create_join_table to create_table

--- db/migrate/20170617123330_create_join_table_user_mountain.rb
+++ db/migrate/20170617123330_create_join_table_user_mountain.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableUserMountain < ActiveRecord::Migration[5.0]
   def change
-    create_join_table :users_mountains do |t|
+    create_table :users_mountains do |t|
       t.integer :user_id
       t.integer :mountain_id
     end
